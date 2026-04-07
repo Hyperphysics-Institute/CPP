c236c5dde71efc4e81eb19af6e168d97d8960e1b
Thomas Lee Abshier, ND <49974902+tlabshier@users.noreply.github.com>
2026-04-06 18:56:06 -0600

    Update .py code for vertex generation

--- series_standard_model/notebooks/nb02_SM8_generate_600cell.py
+++ series_standard_model/notebooks/nb02_SM8_generate_600cell.py
@@ -6,26 +6,42 @@ invphi = 1 / phi
 
 vertices = []
 
-# Group A: (±1, 0, 0, 0)
+# Group A: 8 vertices (±1, 0, 0, 0)
 for i in range(4):
     for s in [+1, -1]:
         v = [0, 0, 0, 0]
         v[i] = s
         vertices.append(v)
 
-# Group B: (±1/2, ±1/2, ±1/2, ±1/2) with even number of minus signs
+# Group B: 16 vertices (±1/2, ±1/2, ±1/2, ±1/2) with even number of minus signs
 for signs in itertools.product([+0.5, -0.5], repeat=4):
     if sum(s < 0 for s in signs) % 2 == 0:
         vertices.append(list(signs))
 
-# Group C: even permutations of (0, ±invphi, ±phi, ±1)
+# Group C: 96 vertices
 base = [0, invphi, phi, 1]
+
+# Even permutations only
+def even_permutations(lst):
+    perms = set(itertools.permutations(lst))
+    even = []
+    for p in perms:
+        # Compute permutation parity
+        inv = 0
+        for i in range(4):
+            for j in range(i+1, 4):
+                if p[i] > p[j]:
+                    inv += 1
+        if inv % 2 == 0:
+            even.append(p)
+    return even
+
 for signs in itertools.product([+1, -1], repeat=3):
     signed = [0, signs[0]*invphi, signs[1]*phi, signs[2]*1]
-    for perm in set(itertools.permutations(signed)):
+    for perm in even_permutations(signed):
         vertices.append(list(perm))
 
 vertices = np.array(vertices)
 print("Total vertices:", len(vertices))
 
-np.savetxt("nb02_SM8_600cell_vertices.txt", vertices)
+np.savetxt("600cell_vertices.txt", vertices)
