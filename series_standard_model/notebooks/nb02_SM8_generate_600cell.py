17e8557bbdaf1cf9a7e1bb38aa1f0fa89f8bfb6e
Thomas Lee Abshier, ND <49974902+tlabshier@users.noreply.github.com>
2026-04-06 19:01:43 -0600

    Refactor vertex generation
    got 208 vertices on last.
    rewrote.

--- series_standard_model/notebooks/nb02_SM8_generate_600cell.py
+++ series_standard_model/notebooks/nb02_SM8_generate_600cell.py
@@ -2,27 +2,29 @@ import numpy as np
 import itertools
 
 phi = (1 + np.sqrt(5)) / 2
-invphi = 1 / phi
 
 vertices = []
 
-# Group 1: 16 vertices of the form (±1, 0, 0, 0) and permutations
+# H4 root system: 120 roots
+# Type 1: 8 roots of the form (±1, 0, 0, 0)
 for i in range(4):
     for s in [+1, -1]:
         v = [0, 0, 0, 0]
         v[i] = s
         vertices.append(v)
 
-# Group 2: 8 vertices of the form (±1/2, ±1/2, ±1/2, ±1/2) with even number of minus signs
+# Type 2: 16 roots of the form (±1/2, ±1/2, ±1/2, ±1/2)
+# with an even number of minus signs
 for signs in itertools.product([+0.5, -0.5], repeat=4):
     if sum(s < 0 for s in signs) % 2 == 0:
         vertices.append(list(signs))
 
-# Group 3: 96 vertices from permutations of (0, ±invphi, ±phi, ±1)
-base = [0, invphi, phi, 1]
+# Type 3: 96 roots of the form (0, ±1/2, ±phi/2, ±1/(2phi))
+# with all permutations
+vals = [0, 0.5, phi/2, 1/(2*phi)]
 
 for signs in itertools.product([+1, -1], repeat=3):
-    signed = [0, signs[0]*invphi, signs[1]*phi, signs[2]*1]
+    signed = [0, signs[0]*0.5, signs[1]*(phi/2), signs[2]*(1/(2*phi))]
     for perm in set(itertools.permutations(signed)):
         vertices.append(list(perm))
 
